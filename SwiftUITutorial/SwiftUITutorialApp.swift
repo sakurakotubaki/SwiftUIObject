07116d8ad983299994f58f9aa45b42fbf76fc835
sakurakotubaki <gitonly543@gmail.com>
2023-11-20 08:02:26 +0900

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by 橋本純一 on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
